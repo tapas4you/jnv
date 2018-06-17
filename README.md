d3d2fa46745435d383d79921d810b0f0b2a48001
Tapas <tapas@paktolus.com>
2018-06-17 19:19:09 +0530

    Final First Commit.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jnv
-JNV website
+to run this application please follow the process.
+1. Install the wamp.
+2. Clone this application.
+3. Import the sql file.
+
--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'jnv_new');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rN*MI^q@EIjO');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
